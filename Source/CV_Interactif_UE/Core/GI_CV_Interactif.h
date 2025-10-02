bd4f44f0354579db67ad2ce816df1a40737e9f35
Justine <209288487+Drifagrim@users.noreply.github.com>
2025-10-02 18:23:54 -0400

    Create Game Instance files

---
+++ Source/CV_Interactif_UE/Core/GI_CV_Interactif.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GI_CV_Interactif.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CV_INTERACTIF_UE_API UGI_CV_Interactif : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
